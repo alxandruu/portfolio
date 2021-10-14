57bc95b2518ea28cc4d263592058d0edc0f7c388
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2021-10-14 20:13:57 +0200

    1v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
+  { path: 'cv', component: CvComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', redirectTo: 'projects', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './cv/header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './cv/content/content.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CvComponent } from './cv/cv.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    ContentComponent,
+    ProjectsComponent,
+    CvComponent,
+    
+
+
+
   ],
   imports: [
     BrowserModule,
