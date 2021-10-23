289f867237c5873a8f7eb9a6db66022af49307e6
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2021-10-23 15:29:22 +0200

    Mobile Responsive

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -13,23 +13,8 @@ export class NavComponent implements OnInit {
   }
 
   public hamburguer(): void {
-    document.querySelector(".hamburguer-bt")?.classList.toggle("on");
+    document.querySelector("#hamb_menu")?.classList.toggle("show");
   }
 
-  public toggleLight(): void {
-    let arr = document.querySelectorAll('.visual_mode');
-    let r: any = document.querySelector(':root');
-    let btnShow: any = document.querySelector('.visual_mode.show');
   
-    if (btnShow.classList.split(' ').includes('light')) {
-      r.style.setProperty('--dark-color', '#f2fbe0');
-      r.style.setProperty('--light-color', '#2a2d34');
-    } else {
-      r.style.setProperty('--dark-color', '#2a2d34');
-      r.style.setProperty('--light-color', '#f2fbe0');
-    }
-    arr[0].classList.toggle('show');
-    arr[1].classList.toggle('show');
-
-  }
 }
